b5e211b2b97cc80026068b9fccf76ac1447a81c5
Rodrigo Bitu <rodrigo.moraes@enacom.com.br>
2023-04-16 14:15:16 -0300

    feat: adjust .gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
 @Module({
   imports: [
     MongooseModule.forRoot(
