3d5049179b71be374827a702d81fcae493ab9a88
Rodrigo Bitu <rodrigo.moraes@enacom.com.br>
2023-04-16 20:48:56 -0300

    feat: add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
